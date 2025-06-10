082b02001495b8ef0f6b6fc78803a0c3760149a3
RT <rapol@fantom.foundation>
2025-06-10 07:50:15 +0000

    add AdvanceEpoch

--- genesis/network/rules.go
+++ genesis/network/rules.go
@@ -2,6 +2,8 @@ package network
 
 import (
 	"fmt"
+	"math/big"
+
 	"github.com/0xsoniclabs/norma/genesistools/genesis"
 	"github.com/0xsoniclabs/sonic/evmcore"
 	"github.com/0xsoniclabs/sonic/gossip/contract/driverauth100"
@@ -9,7 +11,6 @@ import (
 	"github.com/0xsoniclabs/sonic/opera/contracts/driverauth"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/core/types"
-	"math/big"
 )
 
 // ApplyNetworkRules updates the network rules on the network.
@@ -45,6 +46,40 @@ func ApplyNetworkRules(backend ContractBackend, rules genesis.NetworkRules) erro
 
 	if rec.Status != types.ReceiptStatusSuccessful {
 		return fmt.Errorf("failed to update network rules: status: %v", rec.Status)
+	}
+
+	return nil
+}
+
+// AdvanceEpoch trigger the sealing of an epoch and advance the epoch by the given number.
+// The function blocks until the final epoch has been reached
+func AdvanceEpoch(backend ContractBackend, epochIncrement int) error {
+	contract, err := driverauth100.NewContract(driverauth.ContractAddress, backend)
+	if err != nil {
+		return fmt.Errorf("failed to get driver auth contract representation; %v", err)
+	}
+
+	originalRules := opera.FakeNetRules(opera.GetSonicUpgrades())
+
+	// Use Fake ID for the network
+	// Driver owner is the first validator from the list i.e., index 1 (defined in genesis export in genesis.GenerateJsonGenesis)
+	txOpts, err := bind.NewKeyedTransactorWithChainID(evmcore.FakeKey(1), big.NewInt(int64(originalRules.NetworkID)))
+	if err != nil {
+		return fmt.Errorf("failed to create txOpts; %v", err)
+	}
+
+	tx, err := contract.AdvanceEpochs(txOpts, big.NewInt(int64(epochIncrement)))
+	if err != nil {
+		return fmt.Errorf("failed to advance epoch; %v", err)
+	}
+
+	rec, err := backend.WaitTransactionReceipt(tx.Hash())
+	if err != nil {
+		return fmt.Errorf("failed to get receipt; %v", err)
+	}
+
+	if rec.Status != types.ReceiptStatusSuccessful {
+		return fmt.Errorf("failed to advance epoch; receipt status: %v", rec.Status)
 	}
 
 	return nil
