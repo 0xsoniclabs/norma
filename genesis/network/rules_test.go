78de2066562cbab75d71c2a80fcc89bfcf963d46
RT <146690782+rpl-ffl@users.noreply.github.com>
2025-06-11 13:39:56 +0700

    Add AdvanceEpoch to network rule (#67)
    * add AdvanceEpoch
    
    * Update genesis/network/rules.go
    
    Co-authored-by: Kamil Jezek <kamil.jezek@gmail.com>
    
    ---------
    
    Co-authored-by: Kamil Jezek <kamil.jezek@gmail.com>

--- genesis/network/rules_test.go
+++ genesis/network/rules_test.go
@@ -39,5 +39,32 @@ func TestLocalNetworkApplyNetworkRules_Success(t *testing.T) {
 	if err := ApplyNetworkRules(backend, rules); err != nil {
 		t.Errorf("failed to apply network rules: %v", err)
 	}
+}
 
+func TestLocalNetworkAdvanceEpoch_Success(t *testing.T) {
+	t.Parallel()
+
+	baseFee := big.Int{}
+	baseFee.SetInt64(123)
+	header := types.Header{BaseFee: &baseFee}
+
+	bytecode, err := convertContractBytecode(driverauth100.ContractMetaData.Bin)
+	if err != nil {
+		t.Fatalf("failed to decode contract bytecode: %v", err)
+	}
+
+	ctrl := gomock.NewController(t)
+	backend := NewMockContractBackend(ctrl)
+	backend.EXPECT().HeaderByNumber(gomock.Any(), gomock.Any()).Return(&header, nil)
+	backend.EXPECT().SuggestGasTipCap(gomock.Any()).Return(&baseFee, nil)
+	backend.EXPECT().PendingCodeAt(gomock.Any(), gomock.Any()).Return(bytecode, nil)
+	backend.EXPECT().EstimateGas(gomock.Any(), gomock.Any()).Return(uint64(123), nil)
+	backend.EXPECT().PendingNonceAt(gomock.Any(), gomock.Any()).Return(uint64(0), nil)
+	backend.EXPECT().SendTransaction(gomock.Any(), gomock.Any()).Return(nil)
+	backend.EXPECT().WaitTransactionReceipt(gomock.Any()).Return(&types.Receipt{Status: types.ReceiptStatusSuccessful}, nil)
+
+	const epochIncrement = 4
+	if err := AdvanceEpoch(backend, epochIncrement); err != nil {
+		t.Errorf("failed to advance epoch: %v", err)
+	}
 }
