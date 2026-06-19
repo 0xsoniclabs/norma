37e2dd842a385208ef5ff27617eb06c14574c889
Luis Ayuso <luis@ph3.at>
2026-06-19 14:42:29 +0200

    Unify way transactions access chainId

--- driver/network/rules_test.go
+++ driver/network/rules_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/0xsoniclabs/norma/genesis"
-	"github.com/0xsoniclabs/sonic/opera"
 	"github.com/ethereum/go-ethereum/core/types"
 	"go.uber.org/mock/gomock"
 )
@@ -23,7 +22,6 @@ func TestApplyNetworkRules_Success(t *testing.T) {
 	backend.EXPECT().PendingNonceAt(gomock.Any(), gomock.Any()).Return(uint64(0), nil)
 	backend.EXPECT().SendTransaction(gomock.Any(), gomock.Any()).Return(nil)
 	backend.EXPECT().WaitTransactionReceipt(gomock.Any()).Return(&types.Receipt{Status: types.ReceiptStatusSuccessful}, nil)
-	backend.EXPECT().GetNetworkRules("latest").Return(opera.FakeNetRules(opera.GetSonicUpgrades()), nil)
 
 	fee := genesis.BigIntValue(*big.NewInt(456))
 	rules := genesis.NetworkRulesPatch{
